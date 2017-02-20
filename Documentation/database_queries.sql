8be3021e1731af4b967f620d065b07a1b19b664e
grepos11 <gregposteraro@gmail.com>
2017-02-20 15:33:28 -0500

    Login functionality startup

---
+++ Documentation/database_queries.sql
@@ -0,0 +1,3 @@
+create table users (uid VARCHAR(20) NOT NULL, password VARCHAR(20) NOT NULL, email VARCHAR(50));
+
+insert into users (uid, password) values ('grepos','itsadog');
---
+++ src/main/java/com/demo/project/controller/SignUpController.java
@@ -0,0 +1,49 @@
+package com.demo.project.controller;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.demo.project.model.User;
+
+@RestController
+@RequestMapping(value = "signUpLogin")
+public class SignUpController {
+
+//	@RequestMapping(value = "/onSignUp", method = RequestMethod.POST)
+//	ResponseEntity<?> onSignUp() {
+//
+//		System.out.println("In the Controller");
+//		
+//		
+//		ResponseEntity response = null;
+//		return response;
+//	}
+	
+	@RequestMapping(value = "onSignUp", method = RequestMethod.POST)
+	String onSignUp(HttpServletRequest request,
+            HttpServletResponse response, Model model) {
+
+		System.out.println("In the Controller");
+		System.out.println(request.getParameterMap().keySet());
+		System.out.println(request.getParameterMap().values());
+		
+		
+		System.out.println(request.getParameter("firstName"));
+		
+		User newUser = new User();
+		newUser.setFirstName(request.getParameter("firstName"));
+		newUser.setLastName(request.getParameter("lastName"));
+		newUser.setEmail(request.getParameter("email"));
+		newUser.setPassword(request.getParameter("password"));
+		
+		return "This is the Return String";
+	}
+}
