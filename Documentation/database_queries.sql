f3154955928724559b3f74da757af366532c1839
grepos11 <gregposteraro@gmail.com>
2017-02-25 19:43:55 -0500

    DB queries

--- Documentation/database_queries.sql
+++ Documentation/database_queries.sql
@@ -1,3 +1,10 @@
+--MySQL Commands:
+--
+--1.) Initalize:  Go to bin folder of mysql and run as admin in cmd: mysqld --initialize
+--2.) Start DB: Go to bin folder of mysql and run as admin in cmd: mysqld --console
+
+
+
 create table users (uid VARCHAR(20) NOT NULL, password VARCHAR(20) NOT NULL, email VARCHAR(50));
 
 insert into users (uid, password) values ('grepos','itsadog');
