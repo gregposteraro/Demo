7a5a04e4d43e07eafb9d4f93a7a1688e845b3c91
lukeschmader <lukes4@vt.edu>
2017-03-04 21:32:58 -0500

    Font End Map Start

---
+++ src/main/webapp/resources/js/ParkMap/parkMap.js
@@ -0,0 +1,13 @@
+ 
+function init() {
+  // initialisation stuff here
+ var map;
+      function initMap() {
+        map = new google.maps.Map(document.getElementById('map'), {
+          center: {lat: 40.389258, lng: -101.334585},
+          zoom: 5
+
+        });
+      }
+
+  }
